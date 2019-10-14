7ca29aa4d1277969d1d709a07cd6f80751ca1582
Naresh <mnaresh3538@gmail.com>
2019-10-14 11:40:50 +0530

    samosa

--- hungry.py
+++ hungry.py
@@ -1,4 +1,8 @@
 hungry=input(are you hungry?")
 if hungry="yes"
 else:
+   hungry=input(are you hungry)
+     if hungry="yes"
+      iam eating ('pizza')
+    iam eating ('samosa')
    print("do your network")
