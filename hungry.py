4dee18e9227501451234fe38abc7d14e0843b20e
Naresh <mnaresh3538@gmail.com>
2019-10-14 11:29:27 +0530

    water

--- hungry.py
+++ hungry.py
@@ -1,4 +1,9 @@
 hungry=input(are you hungry?")
 if hungry="yes"
 else:
+   thirsty=input('are you thirsty')
+        if thirsty='yes'
+           print("drink water")
    print("do your network")
+
+
